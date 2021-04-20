69657ad91f3e4bcc77e024575aa36bbc29c55ef4
rahmamustafa <56435448+rahmamustafa@users.noreply.github.com>
2021-04-20 23:10:55 +0200

    Add files via upload

---
+++ assignment_1.sh
@@ -0,0 +1,19 @@
+cd 
+cd Desktop
+mkdir Directory
+cd Directory
+mkdir subDir_1
+mkdir subDir_2
+cd subDir_1
+echo '1' > afile1
+echo '2' > file2
+
+echo '3'> file3
+
+cp afile1 ../subDir_2
+cd ../subDir_2
+
+who | wc -l >file-n &&  ls | wc -l >> file-n 
+ls [aA]*
+ls |wc -l
+echo 'compilation succeeds'
