e0130b83758e46e459b299b05776c5d3b3753593
rahmamustafa <56435448+rahmamustafa@users.noreply.github.com>
2021-05-06 21:38:33 +0200

    Update Asignment2.sh

--- sheet2/Asignment2.sh
+++ sheet2/Asignment2.sh
@@ -11,3 +11,16 @@ echo "Enter a temprature in degree celsius"
 read celsius
 fehreheit=`expr $celsius*\9/5+32 | bc`
 echo "Tempreture in fehrenheit= $fehreheit"
+echo " Question 3"
+echo "Enter your gross salary"
+read gross_salary
+if [ $gross_salary -ge 2000 ]
+then
+	salary_after_tax=`expr $gross_salary-0.15*$gross_salary |bc`
+elif [ $gross_salary -ge 1000 -a $gross_salary -lt 2000 ]
+then
+	salary_after_tax=`expr $gross_salary-0.1*$gross_salary |bc`
+else
+	salary_after_tax=$gross_salary
+fi
+echo "Your net salary after tax reduction is $salary_after_tax "
